15255c2f14c9721fedbb46921665c51cdac73b0f
Razvan Stoica <razvan@Razvan>
2024-09-21 12:40:16 +0300

    Setup UART

--- Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.d
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.d
@@ -1,52 +1,52 @@
 Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.o: \
  ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
  ../Inc/stm32f4xx_hal_conf.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Include/core_cm4.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Include/cmsis_version.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Include/cmsis_compiler.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Include/cmsis_gcc.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Include/mpu_armv7.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/core_cm4.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Inc/stm32f4xx_hal_conf.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Include/core_cm4.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Include/cmsis_version.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Include/cmsis_compiler.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Include/cmsis_gcc.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Include/mpu_armv7.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/1_GpioInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/core_cm4.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/cmsis_version.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/cmsis_compiler.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/cmsis_gcc.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/mpu_armv7.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
--- Src/main.c
+++ Src/main.c
@@ -1,57 +1,53 @@
 #include "stm32f4xx_hal.h"
 
-#define BTN_PORT GPIOC
-#define BTN_PIN GPIO_PIN_13
-#define LED_PORT GPIOA
-#define LED_PIN GPIO_PIN_5
+UART_HandleTypeDef huart2;
+void uart_init(void);
 
-//BTN = PC13, BUS = AHB1EN bit0
-//LED = PA5,  BUS = AHB1EN bit2
-
-void pc13_btn_init(void);
-void pa5_led_init(void);
-uint8_t buttonStatus;
-
-int main(){
+int main()
+{
 	HAL_Init();
-	pc13_btn_init();
-	pa5_led_init();
+	uart_init();
 	while(1)
 	{
-		buttonStatus =  HAL_GPIO_ReadPin (BTN_PORT, BTN_PIN);
-		HAL_GPIO_WritePin (LED_PORT, LED_PIN, buttonStatus);
+		//HAL_UART_Transmit ()
 	}
 }
 
-void pc13_btn_init(void){
-	//2.Configure PC13 as input pin
-	//Enable
-	__HAL_RCC_GPIOC_CLK_ENABLE();
+void SysTick_Handler(void)
+{
+	HAL_IncTick();
+}
+
+void uart_init(void)
+{
 
 	GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-	GPIO_InitStruct.Pin   = BTN_PIN;
-	GPIO_InitStruct.Mode  = GPIO_MODE_INPUT;
-	GPIO_InitStruct.Pull  = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-
-	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-}
-void pa5_led_init(void){
-	//1. Configure PA5 as output pin
-	//Enable clock
+	//enable UART pins clock access
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 
-	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	//enable UART module clock access
+	__HAL_RCC_USART2_CLK_ENABLE();
 
-	GPIO_InitStruct.Pin   = LED_PIN;
-	GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull  = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	//Configure pins to act as alternate functions pins(UART)
+	GPIO_InitStruct.Pin       = GPIO_PIN_2|GPIO_PIN_3;
+	GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+	GPIO_InitStruct.Pull 	  = GPIO_NOPULL;
+	GPIO_InitStruct.Speed 	  = GPIO_SPEED_FREQ_VERY_HIGH;
 
 	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-}
-void SysTick_Handler(void){
-	HAL_IncTick();
+
+	//Configure UART module
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Mode = UART_MODE_TX;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+
+	HAL_UART_Init(&huart2 );
+
+
 }
