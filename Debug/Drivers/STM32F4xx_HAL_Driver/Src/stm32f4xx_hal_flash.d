d6462c056be28f4e089e5498a63fe07199666922
Razvan Stoica <83972556+StoicaRazvanCornelius@users.noreply.github.com>
2024-10-05 21:34:12 +0300

    made project buildable from any pc

--- Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.d
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.d
@@ -1,52 +1,52 @@
 Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.o: \
  ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
  ../Inc/stm32f4xx_hal_conf.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/core_cm4.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/cmsis_version.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/cmsis_compiler.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/cmsis_gcc.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/mpu_armv7.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
- /home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Inc/stm32f4xx_hal_conf.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/core_cm4.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/cmsis_version.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/cmsis_compiler.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/cmsis_gcc.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include/mpu_armv7.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
-/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Include/mpu_armv7.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
--- Debug/Drivers/STM32F4xx_HAL_Driver/Src/subdir.mk
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/subdir.mk
@@ -52,7 +52,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Drivers/STM32F4xx_HAL_Driver/Src/%.o Drivers/STM32F4xx_HAL_Driver/Src/%.su Drivers/STM32F4xx_HAL_Driver/Src/%.cyclo: ../Drivers/STM32F4xx_HAL_Driver/Src/%.c Drivers/STM32F4xx_HAL_Driver/Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F411xE -c -I/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/STM32F4xx_HAL_Driver/Inc -I../Inc -I/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Include -I/home/razvan/STM32CubeIDE/workspace_1.16.0/2_UartTX_Pooling/Drivers/CMSIS/Device/ST/STM32F4xx/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../Inc -I"C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/STM32F4xx_HAL_Driver/Inc" -I"C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Include" -I"C:/Users/razvan/Desktop/stm32/repositories/STM32F411RE_GPIO/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Drivers-2f-STM32F4xx_HAL_Driver-2f-Src
 
